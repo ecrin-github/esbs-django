282d2b177185f9a60edf13d5e9358df1708ba660
Sergei Gorianin <frequenteen@gmail.com>
2023-11-14 19:49:54 +0400

    add custom middleware to overwrite invalid token info

--- esbs/settings.py
+++ esbs/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.auth_middleware.CustomAuthMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
---
+++ middleware/auth_middleware.py
@@ -0,0 +1,12 @@
+class CustomAuthMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if (request.META.get('HTTP_AUTHORIZATION') == 'Bearer null' or
+                request.META.get('HTTP_AUTHORIZATION') == 'Bearer'):
+            request.META['HTTP_AUTHORIZATION'] = ''
+
+        response = self.get_response(request)
+        response['Access-Control-Allow-Origin'] = '*'
+        return response
