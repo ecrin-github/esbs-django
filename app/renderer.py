e9711644dfb21dc91c32af61b1a9cbe8b1c07fba
Sergei Gorianin <frequenteen@gmail.com>
2023-11-15 12:21:51 +0400

    fix issue of rendering response while deleting

--- app/renderer.py
+++ app/renderer.py
@@ -7,7 +7,7 @@ TYPES = [str, int, bool, float, None]
 class CustomRenderer(CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         status_code = renderer_context['response'].status_code
-        if type(data) not in TYPES:
+        if type(data) not in TYPES and data is not None:
             data.update({'statusCode': status_code})
 
         return super(CustomRenderer, self).render(data, accepted_media_type, renderer_context)
