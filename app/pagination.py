e5ae6dbfa6e5c125562988193f3d2dd2edf3fc6c
cudillal <leopold.cudilla@ecrin.org>
2024-06-11 14:43:43 +0000

    Change default pagination to have results on a single page

--- app/pagination.py
+++ app/pagination.py
@@ -2,6 +2,7 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 10
+    # Note: removing pagination in settings.py changes the structure of the API response (hence why very large page_size is used here instead)
+    page_size = 1000000000
     page_size_query_param = 'page_size'
-    max_page_size = 10000
+    max_page_size = 1000000000
