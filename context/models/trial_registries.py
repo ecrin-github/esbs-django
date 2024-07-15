821ebbe77b0d50922ed5c93f3fc53a58950c279e
cudillal <leopold.cudilla@ecrin.org>
2024-07-15 09:52:11 +0000

    Fix trial registries model

--- context/models/trial_registries.py
+++ context/models/trial_registries.py
@@ -5,11 +5,11 @@ from django.db import models
 
 
 class TrialRegistries(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    id = models.CharField(primary_key=True, max_length=50, editable=False, unique=True, db_index=True)
     name = models.CharField(max_length=75, db_index=True)
     source = models.CharField(max_length=75, db_index=True)
     description = models.TextField(blank=True, null=True)
-    date_added = models.DateField(default=datetime.date.today, null=True)
+    date_added = models.DateField(default=datetime.date.today)
     list_order = models.IntegerField(default=0, null=True)
 
     class Meta:
