dcf0251280f4634ba030b983aaedf974f16c49e1
cudillal <leopold.cudilla@ecrin.org>
2025-02-19 08:37:25 +0000

    Add data sharing statement in study main details serializer

--- mdm/serializers/study/study_main_details_dto.py
+++ mdm/serializers/study/study_main_details_dto.py
@@ -6,4 +6,4 @@ from mdm.models.study.studies import Studies
 class StudyMainDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Studies
-        fields = ['id', 'sd_sid', 'display_title', 'brief_description']
+        fields = ['id', 'sd_sid', 'display_title', 'brief_description', 'data_sharing_statement']
